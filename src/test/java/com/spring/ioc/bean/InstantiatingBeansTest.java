0a134371d8d90215a4f1ede21a176df91eb06ef1
leibingguang <15990097064@139.com>
2018-07-16 09:46:52 +0800

    commit test

---
+++ src/test/java/com/spring/ioc/bean/InstantiatingBeansTest.java
@@ -0,0 +1,76 @@
+package com.spring.ioc.bean;
+
+import com.spring.ioc.bean.initBeans.Person;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+public class InstantiatingBeansTest
+{
+    private static final String ID = "1";
+
+    static BeanFactory beanFactory;
+    @BeforeClass
+    public static void beforeClass()
+    {
+       beanFactory = new XmlBeanFactory(new ClassPathResource("initBeansApplicationContext.xml"));
+    }
+
+    /**
+     * 无参构造器，属性注入
+     */
+    @Test
+    public void testNoneArgsConstructor()
+    {
+        Person person= (Person)beanFactory.getBean("personWithProperties");
+        String value = person.getId();
+        Assert.assertEquals(value, ID);
+    }
+
+    /**
+     * 构造器注入
+     */
+    @Test
+    public void testArgsConstructor()
+    {
+        Person person= (Person)beanFactory.getBean("personWithArgs");
+        String value = person.getId();
+        Assert.assertEquals(value, ID);
+    }
+
+    /**
+     * c:标签构造器注入
+     */
+    @Test
+    public void testArgsConstructorWithCTags()
+    {
+        Person person= (Person)beanFactory.getBean("personWithCTags");
+        String value = person.getId();
+        Assert.assertEquals(value, ID);
+    }
+
+    /**
+     * 静态工厂方法注入
+     */
+    @Test
+    public void testStaticFactoryMethod()
+    {
+        Person person= (Person)beanFactory.getBean("personWithStaticFactoryMethod");
+        String value = person.getCar().getId();
+        Assert.assertEquals(value, "staticFactoryMethodId");
+    }
+
+    /**
+     * 实例工厂方法注入
+     */
+    @Test
+    public void testInstanceFactoryMethod()
+    {
+        Person person= (Person)beanFactory.getBean("personWithInstanceFactoryMethod");
+        String value = person.getCar().getId();
+        Assert.assertEquals(value, "instanceFactoryMethodId");
+    }
+}
