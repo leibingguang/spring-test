23cf82e6d97ada6ccde9e34e5f0ce8501d8e5711
lei <15990097064@139.com>
2019-08-05 23:22:28 +0800

    test

--- src/test/java/com/spring/ioc/bean/beanPostProcessor/BeanPostProcessorDemo.java
+++ src/test/java/com/spring/ioc/bean/beanPostProcessor/BeanPostProcessorDemo.java
@@ -1,8 +1,10 @@
 package com.spring.ioc.bean.beanPostProcessor;
 
+import com.spring.ioc.bean.beanPostProcessor.postprocessor.MyBeanPostProcessor;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
@@ -15,6 +17,7 @@ public class BeanPostProcessorDemo
     public static void beforeClass()
     {
         beanFactory = new XmlBeanFactory(new ClassPathResource("beanPostProcessor/beanPostProcessor.xml"));
+        ((ConfigurableBeanFactory)beanFactory).addBeanPostProcessor(new MyBeanPostProcessor());
     }
 
     @Test
--- src/test/java/com/spring/ioc/bean/beanPostProcessor/postprocessor/MyBeanPostProcessor.java
+++ src/test/java/com/spring/ioc/bean/beanPostProcessor/postprocessor/MyBeanPostProcessor.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class MyBeanPostProcessor implements BeanPostProcessor {
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("postProcessBeforeInitialization");
+        System.out.println("postProcessBeforeInitialization bean=" + bean + " beanName=" + beanName);
         return bean;
     }
 
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        System.out.println("postProcessAfterInitialization");
+        System.out.println("postProcessAfterInitialization bean=" + bean + " beanName=" + beanName);
         return bean;
     }
 }
